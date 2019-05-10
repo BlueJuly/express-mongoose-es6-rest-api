797b74a99626aa2c6028e86045d894bf8def41a3
Leon Dai <leondai@Leons-MacBook-Pro.local>
2019-05-09 21:00:30 -0400

    fix winston issue and use new JWT secret

--- config/winston.js
+++ config/winston.js
@@ -1,6 +1,30 @@
 const winston = require('winston');
 
-const logger = new (winston.Logger)({
+const logger = winston.createLogger({
+  levels: {
+    trace: 0,
+    input: 1,
+    verbose: 2,
+    prompt: 3,
+    debug: 4,
+    info: 5,
+    data: 6,
+    help: 7,
+    warn: 8,
+    error: 9
+  },
+  colors: {
+    trace: 'magenta',
+    input: 'grey',
+    verbose: 'cyan',
+    prompt: 'grey',
+    debug: 'blue',
+    info: 'green',
+    data: 'grey',
+    help: 'cyan',
+    warn: 'yellow',
+    error: 'red'
+  },
   transports: [
     new (winston.transports.Console)({
       json: true,
